171f76592f2958112868ceed5082ae2e89a24439
rayyh <rayyounghong@gmail.com>
2019-12-19 20:30:31 +0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,31 +2,31 @@
 
 cd "$(dirname "${BASH_SOURCE}")" || return
 
-#git pull origin master;
+git pull origin master
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude "licenses/" \
-		--exclude "resources/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE" \
-		--exclude "brew.sh" \
-		--exclude "macos.sh" \
-		-avh --no-perms . ~
-	source ~/.bash_profile
+    rsync --exclude ".git/" \
+        --exclude "licenses/" \
+        --exclude "resources/" \
+        --exclude ".DS_Store" \
+        --exclude ".osx" \
+        --exclude "bootstrap.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE" \
+        --exclude "brew.sh" \
+        --exclude "macos.sh" \
+        -avh --no-perms . ~
+    source ~/.bash_profile
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+    doIt
 else
-	read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo ""
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
 
 unset doIt
