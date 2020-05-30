9b67313d34bbd0f384577ab91d4bcf8eed492824
rayyh <rayyounghong@gmail.com>
2020-05-30 10:55:40 +0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")" || return
+WORKING_DIR=$(dirname "${BASH_SOURCE[0]}")
+WORKING_DIR=$(cd "$WORKING_DIR" && pwd)
 
 git pull origin master
 
-function doIt() {
+function syncFiles() {
     rsync --exclude ".git/" \
-        --exclude "licenses" \
         --exclude "resources" \
         --exclude ".DS_Store" \
-        --exclude ".zshrc" \
         --exclude "bootstrap.sh" \
         --exclude "README.md" \
         --exclude "LICENSE" \
         --exclude "brew.sh" \
         -avh --no-perms . ~
+    # shellcheck disable=SC1090
     source ~/.bash_profile
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+    syncFiles
 else
     read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
     echo ""
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt
+        syncFiles
     fi
 fi
 
-unset doIt
+unset syncFiles
