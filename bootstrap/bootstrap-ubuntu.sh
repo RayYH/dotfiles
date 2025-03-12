40022348be541d9c0e627f73509fa06c0b5f6ff0
rayyh <rayyounghong@gmail.com>
2025-03-12 22:35:28 +0800

    feat: install miniforge3

--- bootstrap/bootstrap-ubuntu.sh
+++ bootstrap/bootstrap-ubuntu.sh
@@ -227,6 +227,21 @@ function __install_composer() {
 }
 __install_composer
 
+function __install_miniforge() {
+    __echo "Installing Miniforge..."
+    if __command_exists "conda"; then
+        __done "$step"
+        step=$((step + 1))
+        return
+    fi
+    curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh" && \
+        bash Miniforge3-$(uname)-$(uname -m).sh -b
+    rm -f Miniforge3-$(uname)-$(uname -m).sh
+    __done "$step"
+}
+__install_miniforge
+
+
 function __fix_locales() {
     __echo "Fixing locales..."
     DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends tzdata locales
