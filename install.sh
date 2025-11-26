1eed455fd14f14376c9c3525fbe8b97734e00f8d
rayyh <rayyounghong@gmail.com>
2025-11-26 16:39:29 +0800

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -18,14 +18,11 @@ else
     cp "$DOTFILES/.setuprc" "$HOME/.setuprc"
 fi
 
-# make sure .config exists
 mkdir -p "$HOME/.config"
 
-# create backup folder based on current date
 BACKUP_FOLDER="$DOTFILES/backup/$(date +%Y%m%d%H%M%S)"
 mkdir -p "$BACKUP_FOLDER"
 
-# kitty
 if [ -d "$HOME/.config/kitty" ]; then
     mv "$HOME/.config/kitty" "$BACKUP_FOLDER"
 fi
@@ -39,29 +36,24 @@ fi
 rm -rf "$HOME/.tmux.conf"
 ln -s "$DOTFILES/config/tmux/tmux.conf" "$HOME/.tmux.conf"
 
-# nvim
 if [ -d "$HOME/.config/nvim" ]; then
     mv "$HOME/.config/nvim" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.config/nvim"
 ln -s "$DOTFILES/config/nvim" "$HOME/.config/nvim"
 
-# alacritty
 if [ -d "$HOME/.config/alacritty" ]; then
     mv "$HOME/.config/alacritty" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.config/alacritty"
 ln -s "$DOTFILES/config/alacritty" "$HOME/.config/alacritty"
 
-# starship
 if [ -f "$HOME/.config/starship.toml" ]; then
     mv "$HOME/.config/starship.toml" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.config/starship.toml"
 ln -s "$DOTFILES/config/starship/starship.toml" "$HOME/.config/starship.toml"
-# if is ubuntu system, use $DOTFILES/config/starship/ubuntu/starship.toml
-# if is macos system, use $DOTFILES/config/starship/macos/starship.toml
-# if is arch system, use $DOTFILES/config/starship/arch/starship.toml
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if [[ -f "$DOTFILES/config/starship/ubuntu/starship.toml" ]]; then
         rm -rf "$HOME/.config/starship.toml"
@@ -79,72 +71,60 @@ elif [[ "$OSTYPE" == "linux-musl" ]]; then
     fi
 fi
 
-# curl
 if [ -f "$HOME/.curlrc" ]; then
     mv "$HOME/.curlrc" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.curlrc"
 ln -s "$DOTFILES/config/curl/.curlrc" "$HOME/.curlrc"
 
-# wget
 if [ -f "$HOME/.wgetrc" ]; then
     mv "$HOME/.wgetrc" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.wgetrc"
 ln -s "$DOTFILES/config/wget/.wgetrc" "$HOME/.wgetrc"
 
-# shell check
 if [ -f "$HOME/.shellcheckrc" ]; then
     mv "$HOME/.shellcheckrc" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.shellcheckrc"
 ln -s "$DOTFILES/config/shellcheck/.shellcheckrc" "$HOME/.shellcheckrc"
 
-# uv
 if [ -f "$HOME/.config/uv/uv.toml" ]; then
     mv "$HOME/.config/uv/uv.toml" "$BACKUP_FOLDER"
 fi
-# mkdir first
 mkdir -p "$HOME/.config/uv"
 rm -rf "$HOME/.config/uv/uv.toml"
 ln -s "$DOTFILES/config/uv/uv.toml" "$HOME/.config/uv/uv.toml"
 
-# editorconfig
 if [ -f "$HOME/.editorconfig" ]; then
     mv "$HOME/.editorconfig" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.editorconfig"
 ln -s "$DOTFILES/config/editorconfig/.editorconfig" "$HOME/.editorconfig"
 
-# conda
 if [ -f "$HOME/.condarc" ]; then
     mv "$HOME/.condarc" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.condarc"
 ln -s "$DOTFILES/config/conda/.condarc" "$HOME/.condarc"
 
-# ideavimrc
 if [ -f "$HOME/.ideavimrc" ]; then
     mv "$HOME/.ideavimrc" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.ideavimrc"
 ln -s "$DOTFILES/config/jetbrains/.ideavimrc" "$HOME/.ideavimrc"
 
-# gitignore
 if [ -f "$HOME/.gitignore" ]; then
     mv "$HOME/.gitignore" "$BACKUP_FOLDER"
 fi
 rm -rf "$HOME/.gitignore"
 ln -s "$DOTFILES/config/git/.gitignore" "$HOME/.gitignore"
 
-# gitconfig
 if [ -f "$HOME/.gitconfig" ]; then
     mv "$HOME/.gitconfig" "$BACKUP_FOLDER"
 fi
-# copy gitconfig to home
 cp "$DOTFILES/config/git/.gitconfig" "$HOME/.gitconfig"
 
-# scripts
 mkdir -p "$HOME/.local/bin"
 
 rm -rf "$HOME/.local/bin/t"
@@ -165,9 +145,8 @@ esac
 if grep -q "$DOTFILES/setup.bash" "$BASH_PROFILE"; then
     echo "You've already enabled setup"
 else
-    # write empty line first
     echo >>"$BASH_PROFILE"
-    # shellcheck disable=SC2016
+    # shellcheck disable=SC2016,SC2086
     echo "test -e "$DOTFILES/setup.bash" && source "$DOTFILES/setup.bash"" >>"$BASH_PROFILE"
     echo "setup done, restart your terminal!!"
 fi
