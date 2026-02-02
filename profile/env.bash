78ddfe7c7df8d118774b5544e4ec764b3cbe1532
rayyh <rayyounghong@gmail.com>
2026-02-02 14:59:59 +0800

    feat: remove nix support

--- profile/env.bash
+++ profile/env.bash
@@ -55,9 +55,9 @@ export PATH=$VCPKG_ROOT:$PATH
 [ -x "$HOME/miniforge3/bin/conda" ] && eval "$("$HOME/miniforge3/bin/conda" shell.bash hook)"
 
 # Nix
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-fi
+# if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+#  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+# fi
 
 # avoid duplicate path
 PATH=$(printf "%s" "$PATH" | awk -v RS=':' '!a[$1]++ { if (NR > 1) printf RS; printf $1 }')
